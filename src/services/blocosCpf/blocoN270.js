9073d7d80d3314c50d93f8da222798f29b2e87de
fernanda.oliveira <fernandaos12@gmail.com>
2023-09-26 22:29:24 -0300

    Ajuste if bloco N270

--- src/services/blocosCpf/blocoN270.js
+++ src/services/blocosCpf/blocoN270.js
@@ -7,41 +7,45 @@ const { resultserasa } = require('../serasaService')
 const consultaN270 = resultserasa
 
 const tamanhoBloco = 115
-const indexBloco = consultaN270.IndexOf('N270')
+const indexBloco = consultaN270.indexOf('N270')
 
-var blocoN270 = consultaN270.substring(indexBloco, tamanhoBloco)
+if (indexBloco !== -1) {
+  var blocoN270 = consultaN270.substring(indexBloco, tamanhoBloco)
 
-var subtipo = blocoN270.substring(5, 2)
-if (subtipo === '00') {
-  var dataOcorrencia = blocoN270.substring(7, 8)
-  var numCheque = blocoN270.substring(15, 10)
-  var alineaCheque = blocoN270.substring(25, 5)
-  var qdadeCCF = blocoN270.substring(30, 5)
-  var valor = blocoN270.substring(35, 15)
-  var numeroBanco = blocoN270.substring(50, 3)
-  var nomeBanco = blocoN270.substring(53, 14)
-  var agencia = blocoN270.substring(67, 4)
-  var cidadeAgencia = blocoN270.substring(71, 30)
-  var ufagencia = blocoN270.substring(101, 2)
-  var reservadoSerasa = blocoN270.substring(103, 10)
-  var reservado = blocoN270.substring(113, 3)
-}
+  var subtipo = blocoN270.substring(5, 2)
+  if (subtipo === '00') {
+    var dataOcorrencia = blocoN270.substring(7, 8)
+    var numCheque = blocoN270.substring(15, 10)
+    var alineaCheque = blocoN270.substring(25, 5)
+    var qdadeCCF = blocoN270.substring(30, 5)
+    var valor = blocoN270.substring(35, 15)
+    var numeroBanco = blocoN270.substring(50, 3)
+    var nomeBanco = blocoN270.substring(53, 14)
+    var agencia = blocoN270.substring(67, 4)
+    var cidadeAgencia = blocoN270.substring(71, 30)
+    var ufagencia = blocoN270.substring(101, 2)
+    var reservadoSerasa = blocoN270.substring(103, 10)
+    var reservado = blocoN270.substring(113, 3)
+  }
 
-if (subtipo === '90') {
-  var totalOcorrencias = blocoN270.substring(7, 5)
-  var dataOcorrenciaMaisAntiga = blocoN270.substring(12, 8)
-  var dataOcorrenciaMaisRecente = blocoN270.substring(20, 8)
-  if (dataOcorrenciaMaisRecente) {
-    var numeroBanco = blocoN270.substring(28, 3)
-    var agencia = blocoN270.substring(31, 4)
-    var nomeFantasiaBanco = blocoN270.substring(35, 12)
+  if (subtipo === '90') {
+    var totalOcorrencias = blocoN270.substring(7, 5)
+    var dataOcorrenciaMaisAntiga = blocoN270.substring(12, 8)
+    var dataOcorrenciaMaisRecente = blocoN270.substring(20, 8)
+    if (dataOcorrenciaMaisRecente) {
+      var numeroBanco = blocoN270.substring(28, 3)
+      var agencia = blocoN270.substring(31, 4)
+      var nomeFantasiaBanco = blocoN270.substring(35, 12)
+      var reservado = blocoN270.substring(47, 69)
+    }
+  }
+
+  if (subtipo === '99') {
+    var msg = blocoN270.substring(7, 40)
     var reservado = blocoN270.substring(47, 69)
   }
-}
-
-if (subtipo === '99') {
-  var msg = blocoN270.substring(7, 40)
-  var reservado = blocoN270.substring(47, 69)
+} else {
+  console.log('sem dados ')
 }
 
 module.exports = {
